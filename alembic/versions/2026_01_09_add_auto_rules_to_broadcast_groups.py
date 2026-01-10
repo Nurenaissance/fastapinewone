84d71e0c0d1c3ea7571678668782b1717a3b4f3d
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2026-01-11 04:39:48 +0530

    -09/01/26

--- alembic/versions/2026_01_09_add_auto_rules_to_broadcast_groups.py
+++ alembic/versions/2026_01_09_add_auto_rules_to_broadcast_groups.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '2026_01_09_auto_rules'
-down_revision: Union[str, None] = '2026_01_06_create_flow_data_table'
+down_revision: Union[str, None] = 'flow_data_001'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
